d556ede11acb9daf1c96551d64a74191683725f8
Marcos Gabarda <marcos@cuble.es>
2013-03-07 14:13:56 +0100

    cleaning code and adding support for FontAwesome, issue #1

--- application/config/facebook.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Configuracion para Facebook Connect.
- * 
- */
-$config['facebook_app_id'] = '';
-$config['facebook_app_secret'] = '';
-$config['facebook_callback'] = '/connect/fb/login';
